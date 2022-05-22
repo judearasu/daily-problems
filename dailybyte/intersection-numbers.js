9145218c4871d9e5ba6311584609f9b72f3f5d08
Jude Arasu <judearasu@gmail.com>
2022-05-22 16:54:53 -0400

    KHARMA-12 db(): intersection of numbers - optimized approach

--- dailybyte/intersection-numbers.js
+++ dailybyte/intersection-numbers.js
@@ -10,17 +10,31 @@ nums1 = [2, 4, 6, 8], nums2 = [1, 3, 5, 7], return []
  */
 
 var intersectionNumbers = function(nums1, nums2){
-    const intersect = [];
+    // const intersect = [];
 
-    for(let x=0; x < nums2.length; x++){
-        if(nums1.includes(nums2[x])){
-            if(!intersect.includes(nums2[x])){
-                intersect.push(nums2[x]);
-            }
+    // for(let x=0; x < nums2.length; x++){
+    //     if(nums1.includes(nums2[x])){
+    //         if(!intersect.includes(nums2[x])){
+    //             intersect.push(nums2[x]);
+    //         }
+    //     }
+    // }
+    // return intersect;
+
+    // Optimized approach
+    let map = new Map();
+    const result = [];
+
+    for(let char of nums1){
+       map.set(char, true);
+    }
+    for(let char of nums2){
+        if(map.has(char) && !result.includes(char)){
+            result.push(char);
         }
     }
-    return intersect;
+    return result;
 }
 
-nums1 = [2, 4, 6, 8]; nums2 = [1, 3, 5, 7];
+nums1 = [1, 2, 3, 3]; nums2 = [3,3];
 console.log(intersectionNumbers(nums1, nums2));
