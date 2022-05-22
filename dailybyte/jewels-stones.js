6ff2a419ccd9ba7fae69bb2e0d9fdd854bf00332
Jude Arasu <judearasu@gmail.com>
2022-05-22 14:28:53 -0400

    KHARMA-9 (db): jewels and stones - optimized approach

--- dailybyte/jewels-stones.js
+++ dailybyte/jewels-stones.js
@@ -8,21 +8,37 @@ jewels = "AYOPD", stones = "ayopd", return 0
 **/
 
 var jewelStones = function (jewel, stones) {
-  if (!jewel || !stones) {
-    return false;
-  }
+  // if (!jewel || !stones) {
+  //   return false;
+  // }
+  // let count = 0;
+  // for (let x = 0; x < jewel.length; x++) {
+  //   for (let y = 0; y < stones.length; y++) {
+  //       if(jewel[x] === stones[y]){
+  //           count +=1;
+  //       }
+  //   }
+  // }
+  // return count;
+  let map = new Map();
   let count = 0;
-  for (let x = 0; x < jewel.length; x++) {
-    for (let y = 0; y < stones.length; y++) {
-        if(jewel[x] === stones[y]){
-            count +=1;
-        }
+  for(let char of stones){
+    if(map.get(char)){
+      map.set(char, map.get(char)+1)
+    }else{
+      map.set(char, 1);
+    }
+  }
+
+  for(let [key, value] of map){
+    if(jewel.indexOf(key) > -1){
+      count +=value;
     }
   }
   return count;
 };
 
-jewel = "AYOPD", stones = "ayopd"
+jewel = "abc", stones = "ac"
 console.log(jewelStones(jewel, stones));
 
 // jewels = "abc", stones = "ac", return 2
