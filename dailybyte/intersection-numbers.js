5dce3ef18e840f6f41c4899314824809daa15e1f
Jude Arasu <judearasu@gmail.com>
2022-05-22 16:41:40 -0400

    KHARMA-12 db(): intersection of numbers - brute force

---
+++ dailybyte/intersection-numbers.js
@@ -0,0 +1,26 @@
+/**
+ * This question is asked by Google. Given two integer arrays, return their intersection.
+Note: the intersection is the set of elements that are common to both arrays.
+
+Ex: Given the following arrays...
+
+nums1 = [2, 4, 4, 2], nums2 = [2, 4], return [2, 4]
+nums1 = [1, 2, 3, 3], nums2 = [3, 3], return [3]
+nums1 = [2, 4, 6, 8], nums2 = [1, 3, 5, 7], return []
+ */
+
+var intersectionNumbers = function(nums1, nums2){
+    const intersect = [];
+
+    for(let x=0; x < nums2.length; x++){
+        if(nums1.includes(nums2[x])){
+            if(!intersect.includes(nums2[x])){
+                intersect.push(nums2[x]);
+            }
+        }
+    }
+    return intersect;
+}
+
+nums1 = [2, 4, 6, 8]; nums2 = [1, 3, 5, 7];
+console.log(intersectionNumbers(nums1, nums2));
